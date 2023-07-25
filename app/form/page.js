014f2bb8e8587aaaddd28a42826bb5a3f665a066
Lupe <lupemaydana@gmail.com>
2023-07-25 11:33:05 -0400

    feat: receive data from a form and process in the server

---
+++ app/form/page.js
@@ -0,0 +1,63 @@
+'use client'
+
+import styles from './page.module.css'
+export default function PageWithJSbasedForm() {
+  // Handles the submit event on form submit.
+  const handleSubmit = async (event) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault()
+
+    // Get data from the form.
+    const data = {
+      action: event.target.action.value,
+      role: event.target.role.value,
+      message: event.target.message.value,
+    }
+
+    // Send the data to the server in JSON format.
+    const JSONdata = JSON.stringify(data)
+
+    // API endpoint where we send form data.
+    const endpoint = '/api/form'
+
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: 'POST',
+      // Tell the server we're sending JSON.
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    }
+
+    // Send the form data to our forms API on Vercel and get a response.
+    const response = await fetch(endpoint, options)
+
+    // Get the response data from server as JSON.
+    // If server returns the name submitted, that means the form works.
+    const result = await response.json()
+    alert(`Your message has been sent!`)
+  }
+
+  return (
+    // We pass the event to the handleSubmit() function on submit.
+
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="action">Action:</label>
+      <select name="action" id="action">
+        <option value="write">Write Message</option>
+      </select>
+      <br />
+      <label htmlFor="role">Role:</label>
+      <input type="text" id="role" name="role" required />
+      <br />
+      <label htmlFor="message">Message:</label>
+      <textarea id="message" name="message" required />
+      <br />
+      <button type="submit">Send</button>
+    </form>
+  )
+}
+
